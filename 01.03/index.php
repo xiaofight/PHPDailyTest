93988fa348aa696a26297568881c06253317e352
jzm <13315082456@163.com>
2019-01-03 09:13:28 +0800

    0103日考

---
+++ 01.03/index.php
@@ -0,0 +1,31 @@
+<?php 
+	function GetUglyNumber_Solution($index){
+		while ($index%2==0) {
+			$index/=2;
+		}
+		while ($index%3==0) {
+			$index/=3;
+		}
+		while ($index%5==0) {
+			$index/=5;
+		}
+		if($index==1){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	$max = 1;
+	$n = 0;
+	for ($i=1;;$i++) { 
+		if(GetUglyNumber_Solution($i)){
+			$n++;
+			if($n==$max){ 
+				echo $n."位的丑数是".$i;
+				break;
+			}
+		}
+		
+	}
+	
+ ?>
